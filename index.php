7080bc3e42a7a50fdb65d8f7a78d3f4415c67c78
AnaVitoria1604 <ana.lourenco.senai404@gmail.com>
2024-08-02 10:33:50 -0300

    Add files via upload
    trabalho php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$nome = 'Ana '; /* Declarando variasveis */
+$escola = 'SESI Cerquilho ';
+$CE = 428 ;
+$curso = 'Desenvolvimento de Sistemas ';
+$media = 9.5 ;
+
+echo 'ola mundo, bem-vindo ao SESI SENAI! <br>'; // Texto de "titulo"
+
+echo "$nome <br>"; /* Chamando as variaveis */
+echo "$escola <br>";
+echo "$CE <br>";
+echo "$curso <br>";
+echo "$media <br>";
+
+echo "ola $nome, bem-vindo ao $escola $CE no curso de $curso"; // exemplo de uma linha de texto utilizando as variasveis
+?>
